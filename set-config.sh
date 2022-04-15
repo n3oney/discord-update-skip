2c62ac14a38f5087f9f246d1bb15fdbaefe8cf37
n3oney <neoney@pm.me>
2022-04-15 21:19:42 +0200

    feat: add more verbose logging

--- set-config.sh
+++ set-config.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-CONFIG_FILE=~/.config/discord/settings.json
+CONFIG_DIR=~/.config/discord
+CONFIG_FILE="$CONFIG_DIR"/settings.json
 
-mkdir -p $(dirname "$CONFIG_FILE")
+mkdir -p "$CONFIG_DIR"
 
 CONFIG=$(cat "$CONFIG_FILE" 2>/dev/null)
-: "${CONFIG:={\}}"
+
+if [ -z "$CONFIG" ]; then
+  echo "Your Discord client hasn't created a config file yet. I'll create a new one for you."
+  echo "Make sure you're using the correct script for your Discord version."
+  CONFIG='{}'
+fi
 
 MODIFIED_CONFIG=$(echo $CONFIG | jq ".SKIP_HOST_UPDATE = true")
 
+echo "New config file:"
+echo $(jq -C <<< $MODIFIED_CONFIG)
+
+mv "$CONFIG_FILE" "$CONFIG_DIR"/settings.json.old 2>/dev/null # Backup old config file (if it exists)
 echo $MODIFIED_CONFIG > "$CONFIG_FILE"
+echo "Saved in $CONFIG_FILE"
 
